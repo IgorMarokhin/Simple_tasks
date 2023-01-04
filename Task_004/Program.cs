59c82cabef5fca8aa7922bb3b2103ac923cf3b24
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-04 16:56:37 +0300

    Добавлено решение задачи 4

---
+++ Task_004/Program.cs
@@ -0,0 +1,17 @@
+﻿// Составить программу вывода на экран числа, вводимого с клавиатуры. //
+// Выводимому числу должно предшествовать сообщение «Вы ввели число». //
+
+int userInput;
+
+try
+{
+    Console.Write("Введите целое число: ");
+    userInput = int.Parse(Console.ReadLine() ?? "");
+}
+catch(Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+    return;
+}
+
+Console.WriteLine($"Вы ввели число {userInput}");
