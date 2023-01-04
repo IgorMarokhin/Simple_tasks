1da61bc6f78cc8c9a71142887ee817bccf8865a6
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-04 13:43:55 +0300

    Добавлено решение задачи 1

---
+++ Task_001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Составить программу вывода на экран в одну строку четырех любых чисел с тремя пробелами между ними. //
+Random rnd = new Random();
+
+int number = rnd.Next(1,10);
+int number1 = rnd.Next(1,10);
+int number2 = rnd.Next(1,10);
+int number3 = rnd.Next(1,10);
+
+Console.WriteLine($"{number} {number1} {number2} {number3}");
