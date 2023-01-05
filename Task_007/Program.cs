32f08db4b6b451e8a21cf44d8fe8e54cbd8e7477
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-05 12:56:09 +0300

    Добавлено решение задачи 7

---
+++ Task_007/Program.cs
@@ -0,0 +1,12 @@
+﻿// Ввести любой символ и определить его порядковый номер, а также указать предыдущий и последующий символы. //
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char simbol = Console.ReadLine()[0];
+        Console.WriteLine("Предыдущий символ: {0}", (char)(simbol - 1));
+        Console.WriteLine("Следующий символ: {0}", (char)(simbol + 1));
+        Console.ReadKey(false);
+    }
+}
